bece7a8221493f8853b0d07d8f864ddb872ab082
sfrei <samuel@rsfrei.de>
2020-10-22 19:05:56 +0200

    Fix youtube track format resolving
    - Was not always (mostly never) choosing the best quality

--- library/src/main/java/io/sfrei/tracksearch/utils/TrackFormatUtility.java
+++ library/src/main/java/io/sfrei/tracksearch/utils/TrackFormatUtility.java
@@ -6,6 +6,7 @@ import io.sfrei.tracksearch.tracks.metadata.FormatType;
 import io.sfrei.tracksearch.tracks.metadata.YouTubeTrackFormat;
 import lombok.extern.slf4j.Slf4j;
 
+import java.util.List;
 import java.util.concurrent.atomic.AtomicReference;
 
 @Slf4j
@@ -14,38 +15,55 @@ public class TrackFormatUtility {
     public static YouTubeTrackFormat getBestTrackFormat(YouTubeTrack youtubeTrack, boolean includeVideo)
             throws TrackSearchException {
 
-        AtomicReference<YouTubeTrackFormat> format = new AtomicReference<>(null);
-        for (YouTubeTrackFormat trackFormat : youtubeTrack.getTrackInfo().getFormats()) {
-            if (format.get() == null) {
-                if (trackFormat.getAudioQuality() == null || trackFormat.getAudioSampleRate() == null)
-                    continue;
-                if (trackFormat.getFormatType().equals(FormatType.Unknown))
-                    continue;
-                if (!trackFormat.getFormatType().equals(FormatType.Audio) && !includeVideo)
-                    continue;
+        final AtomicReference<YouTubeTrackFormat> bestFormat = new AtomicReference<>(null);
+        final List<YouTubeTrackFormat> formats = youtubeTrack.getTrackInfo().getFormats();
+        for (YouTubeTrackFormat trackFormat : formats) {
 
-                format.set(trackFormat);
-            }
+            final FormatType formatType = trackFormat.getFormatType();
 
-            String currentAudioQuality = format.get().getAudioQuality();
-            String anotherAudioQuality = trackFormat.getAudioQuality();
-            if (YoutubeAudioQualities.audioQualityBetter(currentAudioQuality, anotherAudioQuality)) {
-                format.set(trackFormat);
+            if (formatType == null || formatType.equals(FormatType.Unknown))
+                continue;
+
+            if (!formatType.equals(FormatType.Audio) && !includeVideo)
+                continue;
+
+            if (trackFormat.getAudioQuality() == null && !includeVideo) {
                 continue;
             }
 
-            int currentSampleRate = Integer.parseInt(format.get().getAudioSampleRate());
-            int anotherSampleRate = Integer.parseInt(trackFormat.getAudioSampleRate());
-            if (currentSampleRate < anotherSampleRate) {
-                format.set(trackFormat);
+            if (bestFormat.get() == null) {
+                bestFormat.set(trackFormat);
+                continue;
+            }
+
+            final String currentAudioQuality = bestFormat.get().getAudioQuality();
+            final String anotherAudioQuality = trackFormat.getAudioQuality();
+
+            final boolean sameQuality = YoutubeAudioQualities.audioQualitySame(currentAudioQuality, anotherAudioQuality);
+
+            if (!sameQuality) {
+                if (YoutubeAudioQualities.audioQualityBetter(currentAudioQuality, anotherAudioQuality)) {
+                    bestFormat.set(trackFormat);
+                    continue;
+                }
+            }
+
+            if (trackFormat.getAudioSampleRate() == null)
+                continue;
+
+            final int currentSampleRate = Integer.parseInt(bestFormat.get().getAudioSampleRate());
+            final int anotherSampleRate = Integer.parseInt(trackFormat.getAudioSampleRate());
+            if (currentSampleRate < anotherSampleRate && sameQuality) {
+                bestFormat.set(trackFormat);
             }
         }
-        if (format.get() != null) {
-            return format.get();
+
+        if (bestFormat.get() != null) {
+            return bestFormat.get();
         }
 
         if (!includeVideo) {
-            log.warn("No audio mime type found for: {} - {} - trying to get alternative",
+            log.warn("No audio mime type found for: {} - {} - trying to get the video as alternative",
                     youtubeTrack.getCleanTitle(), youtubeTrack.getUrl());
             return getBestTrackFormat(youtubeTrack, true);
         }
@@ -58,7 +76,7 @@ public class TrackFormatUtility {
         MEDIUM("AUDIO_QUALITY_MEDIUM"),
         HIGH("AUDIO_QUALITY_HIGH"); //never seen
 
-        String qualityId;
+        final String qualityId;
 
         YoutubeAudioQualities(String qualityId) {
             this.qualityId = qualityId;
@@ -70,15 +88,21 @@ public class TrackFormatUtility {
 
         private static int getOrdinalForQuality(String qualityId) {
             for (YoutubeAudioQualities quality : YoutubeAudioQualities.values()) {
-                if(quality.getQualityId().equals(qualityId))
+                if (quality.getQualityId().equals(qualityId))
                     return quality.ordinal();
             }
             return -1;
         }
 
+        public static boolean audioQualitySame(String current, String other) {
+            final int currentQuality = YoutubeAudioQualities.getOrdinalForQuality(current);
+            final int otherQuality = YoutubeAudioQualities.getOrdinalForQuality(other);
+            return otherQuality == currentQuality;
+        }
+
         public static boolean audioQualityBetter(String current, String other) {
-            int currentQuality = YoutubeAudioQualities.getOrdinalForQuality(current);
-            int otherQuality = YoutubeAudioQualities.getOrdinalForQuality(other);
+            final int currentQuality = YoutubeAudioQualities.getOrdinalForQuality(current);
+            final int otherQuality = YoutubeAudioQualities.getOrdinalForQuality(other);
             return otherQuality > currentQuality;
         }
     }
