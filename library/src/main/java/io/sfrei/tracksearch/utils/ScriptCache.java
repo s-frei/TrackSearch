386002a5508873713ec8fb27488339af2f4999d8
sfrei <samuel@rsfrei.de>
2021-08-26 19:58:21 +0200

    Increase player script cache

--- library/src/main/java/io/sfrei/tracksearch/utils/ScriptCache.java
+++ library/src/main/java/io/sfrei/tracksearch/utils/ScriptCache.java
@@ -3,9 +3,9 @@ package io.sfrei.tracksearch.utils;
 import java.util.LinkedHashMap;
 import java.util.Map;
 
-public class ScriptCache<K, V> extends LinkedHashMap<K, V> {
+public final class ScriptCache<K, V> extends LinkedHashMap<K, V> {
 
-    private static final byte MAX_SIZE = 20;
+    public static final byte MAX_SIZE = 50;
 
     @Override
     protected boolean removeEldestEntry(Map.Entry<K, V> eldest) {
